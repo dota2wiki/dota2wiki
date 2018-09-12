75342673cbc7ed22730ae558780c255c99ed2d0d
duduluu <duduluu@outlook.com>
2018-09-13 03:40:19 +0800

    fix: locale

--- packages/dota2wiki/src/locale.ts
+++ packages/dota2wiki/src/locale.ts
@@ -9,7 +9,7 @@ Vue.use(VueLocale);
  */
 export const dotaLocale: VueLocale = new VueLocale({
   dicts: {
-    en: async () => import('@dota2wiki/i18n/dist/esm/dota/english'),
+    english: async () => import('@dota2wiki/i18n/dist/esm/dota/english'),
     schinese: async () => import('@dota2wiki/i18n/dist/esm/dota/schinese'),
     tchinese: async () => import('@dota2wiki/i18n/dist/esm/dota/tchinese'),
     japanese: async () => import('@dota2wiki/i18n/dist/esm/dota/japanese'),
