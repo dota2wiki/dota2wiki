e92cc37ea3e11f379cee2569184eeb20d55390f9
duduluu <duduluu@outlook.com>
2018-09-29 03:35:52 +0800

    [huiji-test] WIP

---
+++ packages/@dota2wiki/huiji-test/src/index.ts
@@ -0,0 +1,16 @@
+/**
+ * {{Html:TestApp}}
+ */
+
+const wikiBody: Element | null = document.querySelector('#wiki-body');
+
+if (wikiBody && document.querySelector('#test-app')) {
+  const iFrame: HTMLIFrameElement = document.createElement('iframe');
+  iFrame.setAttribute('id', 'app');
+  iFrame.setAttribute('title', 'dota2 wiki');
+  iFrame.setAttribute('width', '100%');
+  iFrame.setAttribute('height', '920');
+  iFrame.setAttribute('src', 'https://dota2wiki.duduluu.com/wiki/schinese/heroes');
+
+  (wikiBody.parentNode as Node).insertBefore(iFrame, wikiBody);
+}
